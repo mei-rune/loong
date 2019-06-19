755831112bd11cccf6c3218fea0f75639d6afb41
meifakun <runner.mei@gmail.com>
2019-06-19 18:05:26 +0800

    add jaeger

---
+++ jaeger/main.go
@@ -0,0 +1,67 @@
+package jaeger
+
+import (
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/kardianos/osext"
+	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go/config"
+	"github.com/uber/jaeger-lib/metrics/prometheus"
+)
+
+var (
+	serviceName = "loong"
+	gTracer     opentracing.Tracer
+	gCloser     io.Closer
+)
+
+func init() {
+	if name, _ := osext.Executable(); name != "" {
+		name = filepath.Base(name)
+		if name != "" {
+			serviceName = name
+		}
+	}
+
+	if name = os.Getenv("LOONG_TRACING_NAME"); name != "" {
+		serviceName = name
+	}
+
+	metricsFactory := prometheus.New()
+	tracer, closer, err := config.Configuration{
+		ServiceName: serviceName,
+	}.NewTracer(
+		config.Metrics(metricsFactory),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	opentracing.SetGlobalTracer(tracer)
+
+	gTracer = tracer
+	gCloser = closer
+}
+
+func Init(name string) error {
+	if name == "" {
+		name = serviceName
+	}
+
+	metricsFactory := prometheus.New()
+	tracer, closer, err := config.Configuration{
+		ServiceName: name,
+	}.NewTracer(
+		config.Metrics(metricsFactory),
+	)
+	if err != nil {
+		return err
+	}
+
+	gCloser.Close()
+	opentracing.SetGlobalTracer(tracer)
+	gTracer = tracer
+	gCloser = closer
+}
